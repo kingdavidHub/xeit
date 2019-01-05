079ad564ab335363ed9dd12f92a7f2579c98f621
kingdavid <kingigbayilola@gmail.com>
2019-01-05 12:32:31 +0100

    login routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express kingdavid' });
 });
 
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Express kingdavid' });
+});
+
 module.exports = router;
